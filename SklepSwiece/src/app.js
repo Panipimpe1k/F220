1c0fa3e2c511e2b16ece56736a68e6f697f4ead7
FrogMaster <kotypierdoloty@gmail.com>
2025-12-17 13:03:56 +0100

    fixing pathing

--- SklepSwiece/src/app.js
+++ SklepSwiece/src/app.js
@@ -10,6 +10,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
 
 app.use('/', candlesRouter);
 
